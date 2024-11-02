c8d77e7c05c32cdd3d672d271a38e7398fb7e48a
Jorge <groman.ssp@gmail.com>
2024-11-02 01:17:41 -0400

    update Angular 15

--- src/app/app.routers.ts
+++ src/app/app.routers.ts
@@ -19,4 +19,4 @@ const appRoutes: Routes = [
     { path: '**', component: NopagefoundComponent }
 ];
 
-export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true, relativeLinkResolution: 'legacy' } );
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
